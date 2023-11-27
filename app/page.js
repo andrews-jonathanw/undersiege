bf4b12df0e99ae5faeb4305ec9ea0959ab69f217
Jonathan Andrews <andrews.jonathanw@gmail.com>
2023-11-27 14:31:41 -0600

    boilerplate

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='flex flex-col justify-center items-center'>
+      <div className='text-5xl'>
+        Hello World
+      </div>
+    </main>
+  )
+}
